47ad8de4c6bf4e148759cafb9322dbaccbb15464
nitisbig <115392360+nitisbig@users.noreply.github.com>
2025-09-06 23:12:12 +0545

    fix: remove unsupported appwrite API key

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,8 +2,7 @@ import { Client, Databases } from 'appwrite';
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || '')
-  .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY || '');
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || '');
 
 export const databases = new Databases(client);
 export const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '';
