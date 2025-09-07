0014252fb2711d2ce42a8905846b0250ca059bcf
nitisbig <115392360+nitisbig@users.noreply.github.com>
2025-09-07 07:09:41 +0545

    Use Appwrite API key for saving

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,7 +2,8 @@ import { Client, Databases } from 'appwrite';
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || '')
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || '');
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT || '')
+  .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY || '');
 
 export const databases = new Databases(client);
 export const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || '';
